a44c7d63e949180b4dfeb8e97a8fe60c155e702b
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-08-03 14:22:07 +0500

    no gsap

--- src/js/lenis.js
+++ src/js/lenis.js
@@ -1,5 +1,3 @@
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
 import Lenis from 'lenis';
 
 const lenis = new Lenis();
@@ -10,13 +8,5 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
-
-lenis.on('scroll', ScrollTrigger.update);
-
-gsap.ticker.add(time => {
-	lenis.raf(time * 1000);
-});
-
-gsap.ticker.lagSmoothing(0);
 
 export default lenis;
