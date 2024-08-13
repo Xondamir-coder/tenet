a7eec135a91c784e5f066dd4e9a7d8de95797aa6
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-08-13 18:10:47 +0500

    bg updated

--- src/components/BgPattern.vue
+++ src/components/BgPattern.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="pattern__container">
-		<img class="pattern" src="@/assets/big-pattern.avif" alt="" />
+		<img class="pattern" src="@/assets/pattern-top.avif" alt="" />
 		<img class="pattern" src="@/assets/small-pattern.avif" alt="" />
 		<img class="pattern" src="@/assets/small-pattern.avif" alt="" />
 		<img class="pattern" src="@/assets/small-pattern.avif" alt="" />
@@ -13,23 +13,23 @@
 <style lang="scss" scoped>
 .pattern {
 	width: 100%;
-	object-fit: contain;
+	object-fit: cover;
 
 	&__container {
 		cursor: none;
 		position: absolute;
 		inset: 0;
-		// top: 10rem;
 		width: 100%;
 		padding: 0 !important;
 		z-index: 0;
-		filter: opacity(0.1);
 		display: flex;
 		flex-direction: column;
-		gap: 1px;
 		top: 10rem;
-		@media only screen and (max-width: 500px) {
+		filter: opacity(0.1);
+
+		@media only screen and (max-width: 1000px) {
 			top: -8rem;
+			width: 300%;
 		}
 	}
 }
