1086f2ce4075ae2315731da834905e36f08cb7a4
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-08-21 17:23:13 +0500

    scrolling fix

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,19 @@
 </template>
 
 <script setup>
+import { onMounted, onUnmounted } from 'vue';
 import Lang from './components/Lang.vue';
+import lenis from './js/lenis';
+
+const func = () => {
+	lenis.scrollTo(0, { immediate: true });
+};
+onMounted(() => {
+	window.addEventListener('unload', func);
+});
+onUnmounted(() => {
+	window.removeEventListener('unload', func);
+});
 </script>
 
 <style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import lenis from '@/js/lenis';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,5 +12,7 @@ const router = createRouter({
 		}
 	]
 });
+
+router.beforeEach(() => {});
 
 export default router;
