2400980e8a5fca2e3aff2972a513d0186276a940
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-09-17 12:20:49 +0500

    fix in scroll

--- src/components/CursorCircle.vue
+++ src/components/CursorCircle.vue
@@ -9,6 +9,10 @@ import gsap from 'gsap';
 import { onMounted, onUnmounted, ref } from 'vue';
 
 const circleRef = ref(null);
+const circleData = {
+	x: 0,
+	y: 0
+};
 
 // Update mouse position on mousemove
 const detectDevice = () => {
@@ -28,17 +32,30 @@ const detectDevice = () => {
 };
 const isOnPc = detectDevice() == 'PC/Laptop';
 const handleMouseMove = e => {
+	circleData.x = e.clientX;
+	circleData.y = e.clientY;
 	gsap.to(circleRef.value, {
 		duration: 0.1,
 		clipPath: `circle(50px at ${e.pageX + 2}px ${e.pageY - 100}px)`
 	});
 };
+const handleScroll = () => {
+	const { scrollY, scrollX } = window;
+	gsap.to(circleRef.value, {
+		duration: 0.1,
+		clipPath: `circle(50px at ${circleData.x + 2 + scrollX}px ${
+			circleData.y - 100 + scrollY
+		}px)`
+	});
+};
 
 onMounted(() => {
+	window.addEventListener('scroll', handleScroll);
 	window.addEventListener('mousemove', handleMouseMove);
 });
 
 onUnmounted(() => {
+	window.removeEventListener('scroll', handleScroll);
 	window.removeEventListener('mousemove', handleMouseMove);
 });
 </script>
